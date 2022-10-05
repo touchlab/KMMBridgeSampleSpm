f77bb54af10712ab15eba310ed9be2d7b168f295
Michael Friend <michaelf7777@gmail.com>
2022-10-05 14:46:01 -0400

    Initial Commit

---
+++ KMMBridgeSampleSpm/KMMBridgeSampleSpmApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMMBridgeSampleSpmApp.swift
+//  KMMBridgeSampleSpm
+//
+//  Created by Michael Friend on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct KMMBridgeSampleSpmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
